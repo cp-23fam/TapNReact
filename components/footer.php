277859b089edfd762642d53fe28225054710260a
Quentin Mafille <quentin.mafille@ceff.ch>
2024-11-11 07:44:38 +0100

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center">
-    Design par <a href="">Quentin Mafille</a> et développement par <a href="">Fabian Marti</a>
+    Design par <a href="https://www.google.com/search?client=opera&q=Quentin+Mafille&sourceid=opera&ie=UTF-8&oe=UTF-8">Quentin Mafille</a> et développement par <a href="https://www.google.com/search?client=opera&q=Fabian+Marti&sourceid=opera&ie=UTF-8&oe=UTF-8">Fabian Marti</a>
 </footer>
