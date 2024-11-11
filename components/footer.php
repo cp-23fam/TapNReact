76f4acf628df8bb85ca2beef25a8b38145844f88
Quentin Mafille <quentin.mafille@ceff.ch>
2024-11-11 15:27:47 +0100

    <-- Même chose

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,3 @@
 <footer class="text-center color-change-2x">
-    Design par <a href="">Quentin Mafille</a> et développement par <a href="">Fabian Marti</a>
+    Design par <a href="" class="name-footer">Quentin Mafille</a> et développement par <a href="" class="name-footer">Fabian Marti</a>
 </footer>
