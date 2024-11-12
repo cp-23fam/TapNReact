7b7e2265532f1ac3464ab3b44cdc6d3ce6a3eeae
Fabian Marti <fabian.marti@ceff.ch>
2024-11-12 16:19:44 +0100

    My bad

--- index.php
+++ index.php
@@ -1,65 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 
-        <main>
-            <div class="container-flex">
-                <div class="row row-cols-md-5 row-cols-1">
-                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
-                        <div class="card mx-auto" style="width: 18rem; background-color: #E0E0E0 ; margin: 2vh;">
-                            <div class="card-body p-0">
-                                <h3 class="card-title text-center p-3" style="background-color: #A3C2D0;"><a href="./games/missing-dot.php" style="font-weight: bold; text-decoration: none; color: #333333;">JOUER !</a></h3>
-                                <img class="card-img-top p-3" src="./images/missing-dot.png" alt="Image">
-                                <p class="card-text p-3" style="color: #777777; font-weight: bold;">Deux cercles tournants affichent des points de couleurs différentes, mais seulement un point est manquant sur le disque de droite. À vous de le trouver</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
-                        <div class="card mx-auto" style="width: 18rem; background-color: #E0E0E0 ; margin: 2vh;">
-                            <div class="card-body p-0">
-                                <h3 class="card-title text-center p-3" style="background-color: #A3C2D0;"><a href="./games/flash-dot.php" style="font-weight: bold; text-decoration: none; color: #333333;">JOUER !</a></h3>
-                                <img class="card-img-top p-3" src="./images/flash-dot.png" alt="Image">
-                                <p class="card-text p-3" style="color: #777777; font-weight: bold;">Deux cercles clignotent avec des couleurs différentes. Cliquez lorsque les deux cercles ont la même couleur. La rapidité est la clé.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
-                        <div class="card mx-auto" style="width: 18rem; background-color: #E0E0E0 ; margin: 2vh;">
-                            <div class="card-body p-0">
-                                <h3 class="card-title text-center p-3" style="background-color: #A3C2D0;"><a href="./games/missing-color.php" style="font-weight: bold; text-decoration: none; color: #333333;">JOUER !</a></h3>
-                                <img class="card-img-top p-3" src="./images/missing-color.png" alt="Image">
-                                <p class="card-text p-3" style="color: #777777; font-weight: bold;">Des cercles de couleurs différentes sont affichés, a vous de retenir les couleurs et trouer laquelle n’était pas affiché.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
-                        <div class="card mx-auto" style="width: 18rem; background-color: #E0E0E0 ; margin: 2vh;">
-                            <div class="card-body p-0">
-                                <h3 class="card-title text-center p-3" style="background-color: #A3C2D0;"><a href="./games/reaction.php" style="font-weight: bold; text-decoration: none; color: #333333;">JOUER !</a></h3>
-                                <img class="card-img-top p-3" src="./images/reaction.png" alt="Image">
-                                <p class="card-text p-3" style="color: #777777; font-weight: bold;">Un test de réaction, classique mais efficace.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
-                        <div class="card mx-auto" style="width: 18rem; background-color: #E0E0E0 ; margin: 2vh;">
-                            <div class="card-body p-0">
-                                <h3 class="card-title text-center p-3" style="background-color: #A3C2D0;"><a href="./games/infinite-number.php" style="font-weight: bold; text-decoration: none; color: #333333;">JOUER !</a></h3>
-                                <img class="card-img-top p-3" src="./images/INFINITE-NUMBER.png" alt="Image">
-                                <p class="card-text p-3" style="color: #777777; font-weight: bold;">Un nombre est affiché à l’écran, à vous de le retenir. Seulement le nombre augmente d’une dizaine chaque round. Quel sera votre record ?</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
-                        <div class="card mx-auto" style="width: 18rem; background-color: #E0E0E0 ; margin: 2vh;">
-                            <div class="card-body p-0">
-                                <h3 class="card-title text-center p-3" style="background-color: #A3C2D0;"><a href="./games/missing-dot.php" style="font-weight: bold; text-decoration: none; color: #333333;">JOUER !</a></h3>
-                                <img class="card-img-top p-3" src="./images/cat.jpeg" alt="Image">
-                                <p class="card-text p-3" style="color: #777777; font-weight: bold;">Deux cercles tournants affichent des points de couleurs différentes, mais seulement un point est manquant sur le disque de droite. À vous de le trouver</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>   
+<head>
+  <?php require_once('components/head.php') ?>
+  <title>Main</title>
+</head>
 
 <body>
   <?php require_once('components/header.php') ?>
