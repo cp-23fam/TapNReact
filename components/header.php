b5e2354e2fcfb169ba6f4004214d52466939c9fd
Fabian Marti <fabian.marti@ceff.ch>
2024-11-12 13:37:45 +0100

    🔨 Fix header

--- components/header.php
+++ components/header.php
@@ -1,20 +1,19 @@
-<header class="navbar">
-    <div class="container-fluid">
-        <div class="row w-100 text-center text-md-start">
-            <div class="col-6 col-md-2 col-xl-1 links">
-                <a href="/index.php">Accueil</a>
-            </div>
-            <div class="col-6 col-md-2 col-xl-1 links">
-                <a href="/scores.php">Scores</a>
-            </div>
-            <div class="col-6 col-md-2 col-xl-1 links">
-                <a href="/profil.php">Profil</a>
-            </div>
-            <div class="offset-3 col-3 offset-md-4 col-md-2 offset-xl-8 col-xl-1 text-start ms-md-auto links">
-                <button onclick="window.location.href='login.php'" type="button" class="border-0 bg-transparent">
-                    <i class="fa-solid fa-right-to-bracket"></i>
-                </button>
-            </div>
+<header class="bar navbar navbar-expand-md">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#content" aria-controls="content" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="content">
+      <div class="d-flex w-100">
+        <a href="/index.php" class="nav-item px-md-2 me-1">Accueil</a>
+        <a href="/scores.php" class="nav-item px-md-2 me-1">Scores</a>
+        <a href="/profil.php" class="nav-item px-md-2 me-1">Profil</a>
+        <div class="text-end ms-auto me-2">
+          <button onclick="window.location.href='login.php'" type="button" class="border-0 bg-transparent">
+            <i class="fa-solid fa-right-to-bracket"></i>
+          </button>
         </div>
+      </div>
     </div>
+  </div>
 </header>
